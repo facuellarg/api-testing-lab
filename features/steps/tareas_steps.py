34fdd14cb119da763332f677a21d79d94af90dca
facuellarg <32692084+facuellarg@users.noreply.github.com>
2024-12-13 15:49:28 +0000

    implementacion de creacion de tareas

--- features/steps/tareas_steps.py
+++ features/steps/tareas_steps.py
@@ -1,38 +1,77 @@
 import requests
+import json
 import pandas as pd
 from behave import given, when, then
 
 
 @given(u'un nombre "{task}"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given un nombre "tarea01"')
+def step_impl(context, task):
+    context.base_url = "http://localhost:3000/tareas"
+    context.task = task
 
 
 @given(u'una fecha "{date}"')
 def step_impl(context, date):
-    raise NotImplementedError(u'STEP: Given una fecha "01/01/2024"')
+    context.date = date
 
-
-@given(u'un responsables "{responsable}"')
+@given(u'un responsable "{responsable}"')
 def step_impl(context, responsable):
-    raise NotImplementedError(u'STEP: Given un responsable ""')
+    context.responsable = responsable
 
 
-def verificar_si_esta(tuple_values, db_table):
-    return False
+def verificar_si_esta(context):
+    try:
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.get(
+            f'{context.base_url}?task={context.task}',
+            headers=headers
+        )
+        response.raise_for_status
+        return len(response.json()) > 0
+    except requests.exceptions.RequestException as e:
+        print(f"Error al realizar el GET: {e}")
+        return None
 
-def crear_registro(tuple_values, db_table):
-    return
+
+def crear_registro(context):
+    data = {
+        "task":context.task,
+        "date":context.date,
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    try:
+        # Realizar petición POST
+        response = requests.post(
+            context.base_url,
+            data=json.dumps(data),
+            headers=headers
+        )
+        
+        # Verificar si la petición fue exitosa
+        response.raise_for_status()
+        
+        # Retornar datos creados
+        return response.json()
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error al realizar el POST: {e}")
+        return None
+
 
 @when(u'hacen click en crear una tarea')
 def step_impl(context):
-    db_tabla_tarea =pd.read_csv(PATH_DB)
-    ya_esta = verificar_si_esta(tuple_values, db_tabla_tarea)
-    if not ya_esta:
-        crear_registro(tuple_values, db_table)
-
-
+    try:
+        esta = verificar_si_esta(context)
+        assert not esta
+        context.id = crear_registro(context)
+    except requests.exceptions.RequestException as e:
+        (f"Error al realizar el POST: {e}")
+        return None
 
 @then(u'responde el identificador de la tarea "{id}"')
 def step_impl(context, id):
-    raise NotImplementedError(u'STEP: Then responde el identificador de la tarea "1"')
+    assert context.id is not None
