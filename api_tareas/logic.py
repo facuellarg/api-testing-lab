2299d90d447b162f953b3474ce9ea705ded0d161
Rolando Beltran <70067712+rolandobeltran@users.noreply.github.com>
2024-12-13 18:53:00 +0000

    +

--- api_tareas/logic.py
+++ api_tareas/logic.py
@@ -18,7 +18,7 @@ class TaskManager:
         output = db[db["estado"]==estado]
         return output
     
-    def asignar(self,db,tarea, responsable_id):
-        db.loc[db["task"]==tarea:"responsable"]=responsable_id
-        return (db.loc[db["task"]==tarea].reset_index()).iloc[0]
+    def asignar(self,db,id_tarea, responsable_id):
+        db.loc[db["id"]==id_tarea:"responsable"]=responsable_id
+        return (db.loc[db["id"]==id_tarea].reset_index()).iloc[0]
     
--- api_tareas/main.py
+++ api_tareas/main.py
@@ -4,17 +4,20 @@ from logic import TaskManager
 
 app = FastAPI()
 bd_tabla_tareas= pd.DataFrame(columns=TaskManager.DB_COLUMNS)
-
+taskManager=TaskManager()
 
 #filtrar por estado
 @app.get("/tareas")
 async def root(estado):
-    return {"message": "Hello World"}
+    output=taskManager.filtrar(bd_tabla_tareas,estado).to_dict("records")
+    return output
 #crear tarea nueva
 @app.post("/tareas")
 async def root(nombre_tarea : str, fecha_limite : str):
-    return {"message": "Hello World"}
+    output = taskManager.create(nombre_tarea,fecha_limite).to_dict("records")
+    return output
 #asignar tarea a usuario
 @app.patch("/tareas")
-async def root(nombre_tarea : str, id_usuario : str):
-    return {"message": "Hello World"}
+async def root(id_tarea : str, id_usuario : str):
+    output = taskManager.asignar(id_tarea,id_usuario).to_dict()
+    return output
